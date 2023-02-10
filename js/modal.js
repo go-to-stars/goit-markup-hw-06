b1fabe3682a44532ef65f107bf4517bd48b37a92
go-to-stars <121138297+go-to-stars@users.noreply.github.com>
2023-02-11 00:30:36 +0200

    added the phone mask script and refined the modal window script to block scrolling

--- js/modal.js
+++ js/modal.js
@@ -9,6 +9,14 @@
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
+    const isModalOpen =
+      refs.openModalBtn.getAttribute('aria-expanded') === 'true' || false;
+    refs.openModalBtn.setAttribute('aria-expanded', !isModalOpen);
     refs.modal.classList.toggle('is-hidden');
+
+    const scrollLockMethod = !isModalOpen
+      ? 'disableBodyScroll'
+      : 'enableBodyScroll';
+    bodyScrollLock[scrollLockMethod](document.body);
   }
 })();
